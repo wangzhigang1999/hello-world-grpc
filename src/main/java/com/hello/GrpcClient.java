7566207f14ce23471f20fc682a20c3135b602abb
Zhigang <wangzhigang1999@live.cn>
2024-02-24 21:32:51 +0800

    Add gRPC client code to send a request and receive a response

--- src/main/java/com/hello/GrpcClient.java
+++ src/main/java/com/hello/GrpcClient.java
@@ -1,2 +1,20 @@
-package com.hello;public class GrpcClient {
+package com.hello;
+
+import com.hello.grpc.HelloRequest;
+import com.hello.grpc.HelloResponse;
+import com.hello.grpc.HelloServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GrpcClient {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
+        HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
+        HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+                .setFirstName("haha")
+                .setLastName("Monster")
+                .build());
+        System.out.println(helloResponse.getGreeting());
+        channel.shutdown();
+    }
 }
